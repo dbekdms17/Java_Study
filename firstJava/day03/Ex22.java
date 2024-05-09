4c85077ed8cdaa1dc702decb8707fabf429970d8
유요한 <110465572+YuYoHan@users.noreply.github.com>
2024-05-09 16:23:55 +0900

    자바공부
    입력 받아서 if, esle if 공부

---
+++ firstJava/day03/Ex22.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Ex22 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		String str;
+		System.out.println("0~99 사이의 수를 입력");
+		n = sc.nextInt();
+
+		if(n < 0 || n > 99 ) {
+			str = "잘못된 수를 입력";
+			return;
+		}
+
+
+
+
+
+	}
+}
